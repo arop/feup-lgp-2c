427295b6dffb6cabeafb35ab2611fd610b91a66b
LilianaFilipa <lflr94@gmail.com>
2016-04-16 15:38:21 +0100

    Post of employee
    Function to save a new employee to the database. Only required fields

--- iTBirthday/method.js
+++ iTBirthday/method.js
@@ -1,14 +1,14 @@
 module.exports = function(express, app, mongoose, path, nodemailer, CronJob) {
-    //Database
+	//Database
     mongoose.connect('mongodb://localhost/iTBirthday'); // change name of database , local database at the moment
-    var db = mongoose.connection;
-    db.on('error', console.error.bind(console, '[MONGOOSE] Database Connection Error'));
-    db.once('open', function(){
-        console.log("[MONGOOSE] Connected to Database");
-    });
-	
-    //Schemas
-    var AdminSchema = new mongoose.Schema({
+	var db = mongoose.connection;
+	db.on('error', console.error.bind(console, '[MONGOOSE] Database Connection Error'));
+	db.once('open', function(){
+		console.log("[MONGOOSE] Connected to Database");
+	});
+
+	//Schemas
+	var AdminSchema = new mongoose.Schema({
 		username : {type: String, trim: true, required: true},
 		password : {type: String, trim: true, required: true, minlength: 64, maxlength: 64}
 	});
@@ -33,59 +33,60 @@ module.exports = function(express, app, mongoose, path, nodemailer, CronJob) {
 		var birthDate = this.birthDate;
 		var age = today.getFullYear() - birthDate.getFullYear();
 		var m = today.getMonth() - birthDate.getMonth();
-		if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) 
+		if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate()))
 		{
 			age--;
 		}
 		return age;
 	});
-	
+
 	var templateTypes = 'Email SMS Facebook'.split(' ');
 
 	var TemplateSchema = new mongoose.Schema({
 		name : {type: String, enum: templateTypes, required: true, trim: true},
 		path : {type: String, required: true, trim: true}
 	});
-	
+
 	//Create the Models
 	var Admin = mongoose.model('Admin', AdminSchema);
 	var Employee = mongoose.model('Employee', EmployeeSchema);
 	var Template = mongoose.model('Template', TemplateSchema);
-	
+
 	function cleanAdmin(){
 		Admin.remove({}, function(err) {
-		  if (err) {
-			console.log ('[MONGOOSE] Error deleting old date in Admin Doc: ' + err);
-		  }
+			if (err) {
+				console.log ('[MONGOOSE] Error deleting old date in Admin Doc: ' + err);
+			}
 		});
 	}
 
 	function cleanEmployee(){
 		Employee.remove({}, function(err) {
-		  if (err) {
-			console.log ('[MONGOOSE] Error deleting old date in Employee Doc ' + err);
-		  }
+			if (err) {
+				console.log ('[MONGOOSE] Error deleting old date in Employee Doc ' + err);
+			}
 		});
 	}
 
 	function cleanTemplate(){
 		Template.remove({}, function(err) {
-		  if (err) {
-			console.log ('[MONGOOSE] Error deleting old date in Template Doc ' + err);
-		  }
+			if (err) {
+				console.log ('[MONGOOSE] Error deleting old date in Template Doc ' + err);
+			}
 		});
 	}
-	
-    //Access to database
+
+	//Access to database
 	function listAll(param){
 		if(param == 'Employee') var query = Employee.find({});
 		else if(param == 'Admin') var query = Admin.find({});
 		else if(param == 'Template') var query = Template.find({});
 		else return;
-		query.exec(function(err, result){
+
+		return query.exec(function(err, result){
 			if(!err){
 				if(result.length > 0){
-					var resultStr = JSON.stringify(result, undefined, 2);
+                    var resultStr = JSON.stringify(result, undefined, 2);
 					console.log(resultStr);
 				} else{
 					console.log('[MONGOOSE] No Results found! Length: ' + result.length);
@@ -98,7 +99,7 @@ module.exports = function(express, app, mongoose, path, nodemailer, CronJob) {
 
 	function searchByName(param){
 		var query = Employee.find({name: new RegExp(param, "i")});
-		query.exec(function(err, result) { 
+		query.exec(function(err, result) {
 			if (!err) {
 				if(result.length > 0){
 					var resultStr = JSON.stringify(result, undefined, 2);
@@ -111,7 +112,7 @@ module.exports = function(express, app, mongoose, path, nodemailer, CronJob) {
 			};
 		});
 	}
-	
+
 	function checkLogin(username, password){
 		var query = Admin.findOne({'username': username, 'password': password});
 		query.exec(function(err, result){
@@ -129,37 +130,58 @@ module.exports = function(express, app, mongoose, path, nodemailer, CronJob) {
 			}
 		});
 	}
-	
-	//Example of the use of cron
-	//Every day of the week at 16.31.00
-    new CronJob('00 31 16 * * 1-7', function() {
-        console.log('Message');
-    }, null, true, 'Europe/London');
-	
-	/*TESTS
-	cleanAdmin();
-	cleanEmployee();
-	cleanTemplate();
-	var admin = new Admin({username: 'admin', password: '68e656b251e67e8358bef8483ab0d51c6619f3e7a1a9f0e75838d41ff368f728'});
-	admin.save(function (err) {if (err) console.log ('[MONGOOSE] Error saving new admin! ' + err)});
-	checkLogin('admin','68e656b251e67e8358bef8483ab0d51c6619f3e7a1a9f0e75838d41ff368f728');
-	
-	var johndoe = new Employee ({
-	  name: 'John Doe',
-	  birthDate: '1990-01-30',
-	  phoneNumber: '965912228',
-	  email: 'johndoe@itgrow.com',
-	  entryDate: '2014-04-10',
-	  sendMail: true,
-	  sendSMS: false,
-	  facebookPost: false
+
+    //Post of employee only required fields
+	app.post('/post_employee/:name/:birthDate/:phoneNumber/:email/:entryDate/:sendMail/:sendSMS/:facebookPost', function (req,res) {
+        var emp_temp = new Employee ({
+            name: req.params.name,
+            birthDate: req.params.birthDate,
+            phoneNumber: req.params.phoneNumber,
+            email: req.params.email,
+            entryDate: req.params.entryDate,
+            sendMail: req.params.sendMail,
+            sendSMS: req.params.sendSMS,
+            facebookPost: req.params.facebookPost
+        });
+        emp_temp.save(function(err, emp){
+            if ( err )
+                return console.error(err);
+            else
+                return console.log("Employee inserted correctly");
+        });
 	});
 
-	johndoe.save(function (err) {if (err) console.log ('[MONGOOSE] Error saving new employee!' + err)});
 
-	listAll('Employee');
-	listAll('Admin');
-	listAll('Template');
-	searchByName('John Doe');
-	/**/
+	//Example of the use of cron
+	//Every day of the week at 16.31.00
+	new CronJob('00 31 16 * * 1-7', function() {
+		console.log('Message');
+	}, null, true, 'Europe/London');
+
+	/*TESTS
+	 cleanAdmin();
+	 cleanEmployee();
+	 cleanTemplate();
+	 var admin = new Admin({username: 'admin', password: '68e656b251e67e8358bef8483ab0d51c6619f3e7a1a9f0e75838d41ff368f728'});
+	 admin.save(function (err) {if (err) console.log ('[MONGOOSE] Error saving new admin! ' + err)});
+	 checkLogin('admin','68e656b251e67e8358bef8483ab0d51c6619f3e7a1a9f0e75838d41ff368f728');
+
+	 var johndoe = new Employee ({
+	 name: 'John Doe',
+	 birthDate: '1990-01-30',
+	 phoneNumber: '965912228',
+	 email: 'johndoe@itgrow.com',
+	 entryDate: '2014-04-10',
+	 sendMail: true,
+	 sendSMS: false,
+	 facebookPost: false
+	 });
+
+	 johndoe.save(function (err) {if (err) console.log ('[MONGOOSE] Error saving new employee!' + err)});
+
+	 listAll('Employee');
+	 listAll('Admin');
+	 listAll('Template');
+	 searchByName('John Doe');
+	 /**/
 }
