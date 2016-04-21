1b43e8591b146db505f51fdf18380561f9794f92
Luís Magalhães <lmaga801@outlook.com>
2016-04-21 23:44:18 +0100

    Profile Creation Working plus some verifications

--- iTBirthday/app/www/js/new_user.js
+++ iTBirthday/app/www/js/new_user.js
@@ -22,6 +22,9 @@ newProfile.controller('NewUserCtrl', function($scope, $state, $http) {
       console.error('Profile phone number is not valid.');
       return false;
     }
+    else {
+      profileData.phoneNumber = GetFormattedPhoneNumber(profileData.phoneNumber);
+    }
 
     if(profileData.email == undefined) {
       console.error('Profile email is not valid.');
@@ -48,275 +51,318 @@ newProfile.controller('NewUserCtrl', function($scope, $state, $http) {
       return false;
     }
 
-    CreateNewUser(profileData);
+    $http.post('/post_employee', {
+      name: profileData.name,
+      birthDate: new Date(profileData.birthDate),
+      phoneNumber : profileData.phoneNumber,
+      email : profileData.email,
+      entryDate : new Date(profileData.entryDate),
+      sendMail: profileData.sendMail,
+      sendSMS : profileData.sendSMS,
+      facebookPost : profileData.facebookPost
+    }).success(function () {
+      //console.log('New user POST successful');
+      return true;
+    }).error(function (err) {
+      console.log('Error while creating new user: ' + err);
+      return false;
+    });
+
+    $state.go('tabs.dash');
   };
 
-  $scope.on_date_start_edit = function($event) {
-
-    var eventNum = ($event.keyCode >= 48 && $event.key <= 57);
-
-    if(eventNum || $event.key == "-" || $event.key == "Backspace") {
-      var inputElem = $($event.target);
-      var text = String(inputElem.val());
-
-      if(eventNum && text.length == 10) {
-        $event.preventDefault();
-        return false;
-      }
-
+  $scope.on_name_edit_start = function($event) {
+    if($event.key == "Tab" || $event.key == "F5" || $event.key == "Backspace" || $event.key == " ") {
       return true;
     }
+
+    var eventAlpha = ($event.keyCode >= 65 && $event.keyCode <= 122);
+    if(eventAlpha) {
+      return true;
+    }
+
+    $event.preventDefault();
+    return true;
+  };
+
+  $scope.on_date_edit = function($event) {
+    if($event.key == "Tab" || $event.key == "F5") {
+      return true;
+    }
+
+    var eventNum = ($event.keyCode >= 48 && $event.key <= 57);
+    if(!eventNum && !($event.key == "Backspace")) {
+      $event.preventDefault();
+      return false;
+    }
+
+    var inputElem = $($event.target);
+    var text = String(inputElem.val());
+
+    if($event.key == "Backspace") {
+      $event.preventDefault();
+      if(text.length == 5) {
+        text = text.substr(0, 3);
+      }
+      else if(text.length == 8) {
+        text = text.substr(0, 6);
+      }
+      else {
+        text = text.substr(0, text.length - 1);
+      }
+    }
+
+    var parcels = text.split("-");
+
+    if(parcels == undefined || parcels.length == 0) {
+      return false;
+    }
+
+    var finalText = "";
+    if(parcels.length > 0) {
+      if(parcels[0].length < 4) {
+        finalText += parcels[0];
+      }
+      else {
+        var year = ClampYear(parseInt(parcels[0]));
+        finalText += year + "-";
+
+        if(parcels.length > 1) {
+          if(parcels[1].length < 2) {
+            finalText += parcels[1];
+          }
+          else {
+            var month = ClampMonth(parseInt(parcels[1]));
+            if (month < 10) {
+              finalText += "0";
+            }
+            finalText += month + "-";
+
+            if(parcels.length > 2) {
+              if(parcels[2].length < 2) {
+                finalText += parcels[2];
+              }
+              else {
+                var day = ClampDay(parseInt(parcels[2]), month, year);
+                if (day < 10) {
+                  finalText += "0";
+                }
+                finalText += day;
+              }
+            }
+          }
+        }
+      }
+    }
+
+    if(finalText.length > 9) {
+      finalText = finalText.substr(0, 9);
+    }
+
+    inputElem.val(finalText);
+    return true;
+  };
+
+  $scope.on_date_keyup = function($event) {
+    if($event.key == "Tab" || $event.key == "F5") {
+      return true;
+    }
+
+    var eventNum = ($event.keyCode >= 48 && $event.key <= 57);
+    if(!eventNum && $event.key == "Backspace") {
+      $event.preventDefault();
+      return false;
+    }
+
+    var inputElem = $($event.target);
+    var text = String(inputElem.val());
+
+
+    if(text.length > 10) {
+      text = text.substr(0, 10);
+    }
+
+    var parcels = text.split("-");
+
+    if(parcels == undefined || parcels.length == 0) {
+      return false;
+    }
+
+    var finalText = "";
+    if(parcels.length > 0) {
+      if(parcels[0].length < 4) {
+        finalText += parcels[0];
+      }
+      else {
+        var year = ClampYear(parseInt(parcels[0]));
+        finalText += year + "-";
+
+        if(parcels.length > 1) {
+          if(parcels[1].length < 2) {
+            finalText += parcels[1];
+          }
+          else {
+            var month = ClampMonth(parseInt(parcels[1]));
+            if (month < 10) {
+              finalText += "0";
+            }
+            finalText += month + "-";
+
+            if(parcels.length > 2) {
+              if(parcels[2].length < 2) {
+                finalText += parcels[2];
+              }
+              else {
+                var day = ClampDay(parseInt(parcels[2]), month, year);
+                if (day < 10) {
+                  finalText += "0";
+                }
+                finalText += day;
+              }
+            }
+          }
+        }
+      }
+    }
+
+    inputElem.val(finalText);
+    return true;
+  };
+
+  $scope.on_phone_number_edit = function($event) {
 
     if($event.key == "Tab" || $event.key == "F5") {
       return true;
     }
 
-    $event.preventDefault();
-    return false;
-  };
+    var eventNum = ($event.keyCode >= 48 && $event.key <= 57);
+    if(!eventNum && !($event.key == "Backspace")) {
+      $event.preventDefault();
+      return false;
+    }
 
-  $scope.on_date_edited = function($event) {
     var inputElem = $($event.target);
-    var text = String(inputElem.val());
-
-    if(text == undefined || text.length <= 0) {
-      return;
-    }
-
-    if(text.length > 10) {
-      text = text.substr(0, 10);
-    }
+    var text = inputElem.val();
 
     if($event.key == "Backspace") {
+      $event.preventDefault();
       if(text.length == 4) {
-        text = text.substr(0, 3);
+        text = text.substr(0, 2);
       }
-      else if(text.length == 7) {
+      else if(text.length == 8) {
         text = text.substr(0, 6);
       }
+      else {
+        text = text.substr(0, text.length - 1);
+      }
     }
 
-    var values = text.split("-");
-
-    if(values == undefined || values.length == 0) {
-      return;
+    if(text == undefined) {
+      $event.preventDefault();
+      return false;
     }
 
-    var year = parseInt(values[0]);
+    if(eventNum && text.length == 11) {
+      $event.preventDefault();
+      return false;
+    }
+
+    if(text.length > 11) {
+      text = text.substr(0, 11);
+    }
+
     var finalText = "";
-    if(year < 1000) {
-      finalText += year;
-    }
-    else {
-      year = ClampYear(year);
-      finalText += year;
-      finalText += "-";
-    }
+    if(text.length > 0) {
+      finalText += text.substr(0, Math.min(3, text.length));
 
-    if (values.length > 1 && values[1].length > 0) {
-      var month = values[1];
-      var numericMonth = ClampMonth(parseInt(values[1]));
+      if (text.length >= 3) {
+        finalText += " ";
 
-      if(month.length == 1) {
+        if(text.length > 3) {
+          finalText += text.substr(4, Math.min(3, text.length - 4));
 
-        if($event.key == "-") {
-          if(numericMonth > 0 && numericMonth < 10) {
-            finalText += "0";
+          if (text.length >= 7) {
+            finalText += " ";
+
+            if (text.length > 7) {
+              finalText += text.substr(8, Math.min(3, text.length - 8));
+            }
           }
-          finalText += numericMonth;
-          finalText += "-";
         }
-        else {
-          finalText += month;
-        }
-      }
-      else {
-        if(numericMonth < 10) {
-          finalText += "0";
-        }
-        finalText += numericMonth;
-        finalText += "-";
-      }
-    }
-
-    if(values.length > 2 && values[2].length > 0) {
-      var day = values[2];
-      var numericDay = ClampDay(parseInt(values[2]), month, year);
-
-      if(day.length == 1) {
-        finalText += day;
-      }
-      else {
-        if(numericDay < 10) {
-          finalText += "0";
-        }
-        finalText += numericDay;
       }
     }
 
     inputElem.val(finalText);
+    return true;
   };
 
-  $scope.on_date_end_edit = function($event) {
-    var inputElem = $($event.target);
-    var text = String(inputElem.val());
-
-    if(text == undefined || text.length <= 0) {
-      return;
-    }
-
-    if(text.length > 10) {
-      text = text.substr(0, 10);
-    }
-
-    if($event.key == "Backspace") {
-      if(text.length == 4) {
-        text = text.substr(0, 3);
-      }
-      else if(text.length == 7) {
-        text = text.substr(0, 6);
-      }
-    }
-
-    var values = text.split("-");
-
-    if(values == undefined || values.length == 0) {
-      return;
-    }
-
-    var finalText = "";
-
-    var year = "", month = "", day = "";
-    if(values.length > 0) {
-      year = values[0];
-      if(year.length == 4) {
-        finalText += year + "-";
-      }
-      else {
-        finalText = "";
-      }
-    }
-    if(finalText.length > 0 && values.length > 1) {
-      month = values[1];
-      if(month.length == 2) {
-        finalText += month + "-";
-      }
-      else {
-        finalText = "";
-      }
-    }
-    if(finalText.length > 0 && values.length > 2) {
-      day = values[2];
-      if(day.length == 2) {
-        finalText += day;
-      }
-      else {
-        finalText = "";
-      }
-    }
-
-    inputElem.val(finalText);
-  };
-
-  $scope.on_phone_number_start_edit = function($event) {
-
-  };
-
-  $scope.on_phone_number_edited = function($event) {
-
-  };
-
-  var ClampYear = function (year) {
-    if(year < 1940) {
-      year = 1940;
+  var ClampYear = function (numericYear) {
+    if(numericYear < 1940) {
+      numericYear = 1940;
     }
     else {
       var currentYear = new Date().getFullYear();
-      if(year > currentYear) {
-        year = currentYear;
+      if(numericYear > currentYear) {
+        numericYear = currentYear;
       }
     }
 
-    return year;
+    return numericYear;
   };
 
-  var ClampMonth = function (month) {
-    if(month <= 0) {
-      month = 1;
+  var ClampMonth = function (numericMonth) {
+    if(numericMonth <= 0) {
+      numericMonth = 1;
     }
-    else if(month > 12) {
-      month = 12;
+    else if(numericMonth > 12) {
+      numericMonth = 12;
     }
 
-    return month;
+    return numericMonth;
   };
 
-  var ClampDay = function (day, month, year) {
-    if(day <= 0) {
-      day = 1;
+  var ClampDay = function (numericDay, month, year) {
+    if(numericDay <= 0) {
+      numericDay = 1;
     }
-    else if(day > 28){
+    else if(numericDay > 28){
       var numDays = new Date(year, month, 0).getDate();
-      if(day > numDays) {
-        day = numDays;
+      if(numericDay > numDays) {
+        numericDay = numDays;
       }
     }
 
-    return day;
-  };
-
-  var CreateNewUser = function (profileData) {
-
-    console.log(profileData);
-
-    // $http.post('/post_employee', {
-    //   name: 'test',
-    //   birthDate: Date.now(),
-    //   phoneNumber : "123456789",
-    //   email : "email@itgrow.com",
-    //   entryDate : Date.now(),
-    //   sendMail: true,
-    //   sendSMS : false,
-    //   facebookPost : false
-    // }).success(function (data) {
-    //   console.log('New user POST successful');
-    // }).error(function (data) {
-    //   console.log('ERROR: ' + data);
-    //   return false;
-    // });
+    return numericDay;
   };
 
   /**
    * @return {string}
    */
-  var ParseNumberDate = function(text) {
-    var parsedText = '';
+  var GetFormattedPhoneNumber = function(number) {
+    var parcels = number.split(" ");
 
-    for(var i = 0; i < text.length; i++) {
-      if(text[i] >= 0 && text[i] <= 9 || text[i] == '-') {
-        parsedText += text[i];
-      }
+    var finalNumber = "";
+
+    for(var i = 0; i < parcels.length; i++) {
+      finalNumber += parcels[i];
     }
 
-    return parsedText;
+    return finalNumber;
   };
 
   /**
    * @return {boolean}
    */
   var IsProperDate = function(text) {
-    if (text.length != 7) {
+    if (text.length != 10) {
       return false;
     }
 
-    for (var i = 0; i < text.length; i++) {
-      if (i == 2) {
-        if (text[i] != "/") {
-          return false;
-        }
-      }
-      else {
-        if (text[i] < 0 || text[i] > 9) {
+    var parcels = text.split("-");
+
+    for(var i = 0; i < parcels.length; i++) {
+      for (var j = 0; j < parcels[i].length; j++) {
+        if (parseInt(parcels[i][j]) < 0 || parseInt(parcels[i][j]) > 9) {
           return false;
         }
       }
@@ -328,18 +374,22 @@ newProfile.controller('NewUserCtrl', function($scope, $state, $http) {
   /**
    * @return {boolean}
    */
-  var IsProperPhoneNumber = function(text) {
-    if(text.length != 9) {
+  var IsProperPhoneNumber = function (text) {
+    if (text.length != 11) {
       return false;
     }
 
-    for(var i = 0; i < text.length; i++) {
-      if(text[i] < 0 || text[i] > 9) {
-        return false;
+    var parcels = text.split(" ");
+
+    for(var i = 0; i < parcels.length; i++) {
+      for (var j = 0; j < parcels[i].length; j++) {
+        if (parseInt(parcels[i][j]) < 0 || parseInt(parcels[i][j]) > 9) {
+          return false;
+        }
       }
     }
 
     return true;
-  }
+  };
 });
 
