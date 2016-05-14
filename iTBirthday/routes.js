ca7503cca5f920628610009a85e00734375d5896
André Pires <andre.r.o.pires@gmail.com>
2016-05-14 19:37:12 +0100

    dont run website with server

--- iTBirthday/routes.js
+++ iTBirthday/routes.js
@@ -7,7 +7,7 @@ module.exports = function(express, app, path) {
         next();
     });
 
-    app.get('/', function (req, res) {
+    /*app.get('/', function (req, res) {
         res.sendFile(path.join(__dirname+"/app/www/index.html"));
-    });
+    });*/
 }
