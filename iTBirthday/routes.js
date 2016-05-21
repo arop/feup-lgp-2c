01f18e2ea24eea9ee8e092ad1c795fec4fafdf58
André Pires <andre.r.o.pires@gmail.com>
2016-05-21 19:43:00 +0100

    allow deleting in server try #2

--- iTBirthday/routes.js
+++ iTBirthday/routes.js
@@ -3,7 +3,7 @@ module.exports = function(express, app, path) {
 
     app.use(function(req, res, next) {
         res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Methods", "*");
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,UPDATE,POST,DELETE');
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         next();
     });
