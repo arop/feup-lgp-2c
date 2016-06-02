ee1fafdc9e5c66729452102fd9d1ad5079c6ef17
AnaRitaFerreira <ei12052@fe.up.pt>
2016-06-02 11:50:02 +0100

    Age Range Chart added

--- iTBirthday/app/www/js/statistics.js
+++ iTBirthday/app/www/js/statistics.js
@@ -10,7 +10,8 @@ angular.module('itBirthday.statistics', [])
     	$scope.data = [$scope.statistics.MFTotal.Female,$scope.statistics.MFTotal.Male];
     	$scope.backgroundColor = ["#F1F1F2","#ECA04B"];
     	$scope.lineLabels = ["18-21","21-25","25-30","30-40","40+"];
-    	//$scope.lineData = [$scope.statistics.AgeGroups['18to21'],$scope.statistics.AgeGroups['21to25'],$scope.statistics.AgeGroups['25to30'],$scope.statistics.AgeGroups['30to40'],$scope.statistics.AgeGroups['40+']];
+    	$scope.lineData = [[$scope.statistics.AgeGroups['18to21'],$scope.statistics.AgeGroups['21to25'],$scope.statistics.AgeGroups['25to30'],$scope.statistics.AgeGroups['30to40'],$scope.statistics.AgeGroups['40+']]];
+        $scope.lineBackgroundColor = ["#ECA04B"];
         console.log(response);
       });
     }
