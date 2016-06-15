b3e5a57840046fb8fb19c54137b872ffcf2498a4
ee09063 <ee09063@fe.up.pt>
2016-06-15 21:27:23 +0100

    Revert "changed index"
    This reverts commit f2c0f22adaddf0c7a5453162f6e985c308d20952.

--- iTBirthday/app/platforms/browser/www/cordova_plugins.js
+++ iTBirthday/app/platforms/browser/www/cordova_plugins.js
@@ -7,23 +7,12 @@ module.exports = [
         "clobbers": [
             "facebookConnectPlugin"
         ]
-    },
-    {
-        "file": "plugins/ionic-plugin-keyboard/www/browser/keyboard.js",
-        "id": "ionic-plugin-keyboard.keyboard",
-        "pluginId": "ionic-plugin-keyboard",
-        "clobbers": [
-            "cordova.plugins.Keyboard"
-        ],
-        "runs": true
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "com.phonegap.plugins.facebookconnect": "1.0.1.1",
-    "cordova-plugin-whitelist": "1.2.2",
-    "ionic-plugin-keyboard": "2.2.0"
+    "com.phonegap.plugins.facebookconnect": "1.0.1.1"
 }
 // BOTTOM OF METADATA
 });
--- iTBirthday/app/platforms/browser/www/js/login.js
+++ iTBirthday/app/platforms/browser/www/js/login.js
@@ -1,32 +1,24 @@
 angular.module('itBirthday.login', [])
 
-  .controller('LoginCtrl', function ($scope, $state, $http, ionicLoadingService) {
+  .controller('LoginCtrl', function($scope,$state, $http) {
 
     $scope.user = {};
 
-    $scope.errorMessage = '';
-
     //log out
-    $scope.logout = function () {
+    /*$scope.logout = function() {
       if ("session" in localStorage) localStorage.removeItem("session");
-      $state.go('login');
-    };
+    };*/
 
-    $scope.login = function () {
-
-      var user = $scope.user;
-
-      $scope.errorMessage = '';
+    $scope.login = function(user) {
       //console.log("LOGIN user: " + user.username + " - PW: " + user.password);
-      if (user.username == undefined || user.password == undefined) {
-        $scope.errorMessage = 'Erro nos valores inseridos.';
+      if ( user.username == undefined || user.password == undefined){
+        alert("Erro nos valores inseridos.");
         return false;
-      } else {
-        ionicLoadingService.showLoading();
-
+      }else {
+        $state.go('tabs.dash');
         $http.post(serverUrl + '/check_login', {
           username: user.username,
-          password: CryptoJS.SHA256(user.password).toString()
+          password: user.password
         }).success(function (data) {
           console.log('[APP] Login Successful');
 
@@ -34,31 +26,13 @@ angular.module('itBirthday.login', [])
           if ("session" in localStorage) localStorage.removeItem("session");
           localStorage.setItem("session", JSON.stringify(data));
 
-          ionicLoadingService.hideLoading();
           $state.go('tabs.dash');
-          user.password = '';
+          
         }).error(function (data) {
-          $scope.errorMessage = 'Erro nos valores inseridos.';
-          user.password = '';
           console.log('ERROR: ' + data);
-          ionicLoadingService.hideLoading();
           return false;
         });
       }
-    }
-  })
-
-  .directive('ngEnter', function () {
-    return function (scope, element, attrs) {
-      element.bind("keydown keypress", function (event) {
-        if (event.which === 13) {
-          scope.$apply(function () {
-            scope.$eval(attrs.ngEnter, {'event': event});
-          });
-
-          event.preventDefault();
-        }
-      });
     }
   });
 
